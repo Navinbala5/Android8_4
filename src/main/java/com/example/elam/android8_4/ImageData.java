932c3e06609699fbdd7e3c5a6715dcc7d63a3dc7
Navinbala5 <bnavin28@gmail.com>
2017-07-02 20:38:51 +0530

    Add files via upload

---
+++ src/main/java/com/example/elam/android8_4/ImageData.java
@@ -0,0 +1,23 @@
+package com.example.elam.android8_4;
+
+
+public class ImageData {
+    private int imageId;
+    private String description;
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
